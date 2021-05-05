d6c8fb4618ea694e494517414197560a50d16886
as <>
2021-05-05 23:34:06 +0900

    ボールを平面に衝突させる

---
+++ Assets/BallThrower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrower : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ballPrefab;
+
+    //ボールの速さ
+    float shotSpeed = 10.0f;
+
+    void Update()
+    {
+        //画面がタッチされたら処理を行う
+        if (Input.touchCount > 0)
+        {
+            //画面タッチの情報を取得する
+            Touch touch = Input.GetTouch(0);
+
+            //画面タッチの開始時のみ処理を行う
+            if (touch.phase == TouchPhase.Began)
+            {
+                //ボールを生成する
+                GameObject ball = Instantiate(ballPrefab, Camera.main.transform);
+
+                //ボールにカメラ前方への速度を与える
+                ball.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * shotSpeed;
+            }
+        }
+    }
+}
--- Assets/Raycast.cs
+++ Assets/Raycast.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-
 public class Raycast : MonoBehaviour
 {
-    //Raycastを実行するためのクラス
+    //Raycast を実行するためのクラス
     private ARRaycastManager m_RaycastManager;
-
-    //Raycastの結果を格納するList
+    //Raycast の結果を格納する List
     private List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
-
-    //キャタクターのPrefab
+    //キャラクターの Prefab
     [SerializeField]
     private GameObject characterPrefab;
-    
     void Start()
     {
-        //GameObjectにアタッチされているRaycastManagerを取得
+        //GameObject にアタッチされている RaycastManager を取得
         m_RaycastManager = GetComponent<ARRaycastManager>();
     }
-
     void Update()
     {
         //画面がタッチされたら処理を行う
@@ -30,28 +25,25 @@ public class Raycast : MonoBehaviour
         {
             //画面タッチの情報を取得する
             Touch touch = Input.GetTouch(0);
-
             //画面タッチの開始時のみ処理を行う
             if (touch.phase == TouchPhase.Began)
             {
-                //タッチした方向にRayを飛ばし、平面との衝突判定を行う
+                //タッチした方向に Ray を飛ばし、平面との衝突判定を行う
                 if (m_RaycastManager.Raycast(
-                                        touch.position,
-                                        hitResults,
-                                        TrackableType.PlaneWithinPolygon
-                                      ))
+                touch.position,
+                hitResults,
+                TrackableType.PlaneWithinPolygon
+                ))
                 {
-                    //最初に交差した平面から姿勢を取得して、GameObjectを生成する
+                    //最初に交差した平面から姿勢を取得して、GameObject を生成する
                     Pose hitPose = hitResults[0].pose;
                     GameObject character = Instantiate(
-                                                characterPrefab,
-                                                hitPose.position,
-                                                hitPose.rotation
-                        );
+                    characterPrefab,
+                    hitPose.position,
+                    hitPose.rotation
+                    );
                 }
-                
             }
         }
-
     }
 }
